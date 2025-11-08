cdcca76cae0c45c6ced320604e2c5c0e36348977
K-iise <91732817+K-iise@users.noreply.github.com>
2025-11-08 20:41:32 +0900

    feat: setup project

---
+++ src/test/java/woowa/chrono/ChronoApplicationTests.java
@@ -0,0 +1,13 @@
+package woowa.chrono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChronoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
