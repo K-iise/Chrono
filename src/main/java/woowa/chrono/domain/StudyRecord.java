f6920c578122dff25efd964253a70732fbf8bf5b
K-iise <91732817+K-iise@users.noreply.github.com>
2025-11-11 20:08:02 +0900

    refactor: update column mappings

--- src/main/java/woowa/chrono/domain/StudyRecord.java
+++ src/main/java/woowa/chrono/domain/StudyRecord.java
@@ -2,43 +2,39 @@ package woowa.chrono.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import java.time.Duration;
+import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Entity
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Entity
-public class Member {
-
+public class StudyRecord {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "user_id", unique = true, nullable = false)
-    private String userId;
+    @Column(unique = true, nullable = false)
+    private String recordId;
 
-    @Column(name = "user_name")
-    private String userName;
+    @ManyToOne
+    @JoinColumn(name = "memberId")
+    private Member member;
 
-    @Enumerated(EnumType.STRING)
-    @Column(name = "grade")
-    private Grade grade;
+    private Duration sessionTime;
 
-    @Column(name = "usage_time")
-    private Duration usageTime;
+    private Date recordTime;
 
-    @Column(name = "point")
-    int point;
 }
