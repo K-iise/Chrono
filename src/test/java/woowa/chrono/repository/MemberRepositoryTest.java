20b4343b3475ced6a98eb12355f69a50446a57a0
K-iise <91732817+K-iise@users.noreply.github.com>
2025-11-11 16:11:52 +0900

    test: add all MemberRepositoryTest

--- src/test/java/woowa/chrono/repository/MemberRepositoryTest.java
+++ src/test/java/woowa/chrono/repository/MemberRepositoryTest.java
@@ -2,11 +2,15 @@ package woowa.chrono.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import jakarta.transaction.Transactional;
+import java.util.Optional;
+import org.assertj.core.api.Assertions;
 import org.hibernate.annotations.SQLJoinTableRestriction;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import woowa.chrono.domain.Grade;
 import woowa.chrono.domain.Member;
 
 @DataJpaTest
@@ -27,8 +31,48 @@ public class MemberRepositoryTest {
     }
 
     @Test
+    @Transactional
     @DisplayName("멤버 등급 수정 테스트")
     public void modifiedGradeTest(){
+        // given
+        Member member = Member.builder().userId("1234").userName("홍길동").grade(Grade.NEWBIE).build();
+        memberRepository.save(member);
 
+        // when
+        member.setGrade(Grade.REGULAR);
+
+        // then
+        Member found = memberRepository.findById(member.getId()).orElse(null);
+        assertThat(found.getGrade()).isEqualTo(Grade.REGULAR);
+    }
+
+    @Test
+    @DisplayName("멤버 삭제 테스트")
+    public void deleteMemberTest(){
+        // given
+        Member member = Member.builder().userId("abcd").userName("홍길동").grade(Grade.NEWBIE).build();
+        memberRepository.save(member);
+
+        // when
+        memberRepository.delete(member);
+
+        // then
+        Optional<Member> found = memberRepository.findById(member.getId());
+        Assertions.assertThat(found).isEmpty();
+    }
+
+    @Test
+    @DisplayName("유저 ID로 멤버 조회 테스트")
+    public void findByUserIdTest(){
+        // given
+        Member member = Member.builder().userId("1234").userName("홍길동").grade(Grade.NEWBIE).build();
+        memberRepository.save(member);
+
+        // when
+        Optional<Member> found = memberRepository.findByUserId("1234");
+
+        // then
+        Assertions.assertThat(found).isPresent();
+        assertThat(found.get().getUserName()).isEqualTo("홍길동");
     }
 }
