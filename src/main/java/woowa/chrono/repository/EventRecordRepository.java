c6f99c181295352639db1942102b3e1527402c80
K-iise <91732817+K-iise@users.noreply.github.com>
2025-11-12 22:26:04 +0900

    test: add EventRecordRepositoryTest

---
+++ src/main/java/woowa/chrono/repository/EventRecordRepository.java
@@ -0,0 +1,8 @@
+package woowa.chrono.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import woowa.chrono.domain.EventRecord;
+
+public interface EventRecordRepository extends JpaRepository<EventRecord, Long> {
+
+}
---
+++ src/test/java/woowa/chrono/repository/EventRecordRepositoryTest.java
@@ -0,0 +1,57 @@
+package woowa.chrono.repository;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import woowa.chrono.domain.Event;
+import woowa.chrono.domain.EventRecord;
+import woowa.chrono.domain.Member;
+
+@DataJpaTest
+public class EventRecordRepositoryTest {
+
+    @Autowired
+    EventRecordRepository eventRecordRepository;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    EventRepository eventRepository;
+
+    @Test
+    @DisplayName("멤버 이벤트 참여 테스트")
+    public void addEventRecordTest() {
+        // given
+        Member member = Member.builder()
+                .userId("123")
+                .userName("홍길동")
+                .build();
+        Event event = Event.builder()
+                .eventId("12")
+                .content("테스트 이벤트")
+                .admin(member)
+                .startTime(LocalDateTime.MIN)
+                .endTime(LocalDateTime.MAX)
+                .build();
+
+        // when
+        memberRepository.save(member);
+        eventRepository.save(event);
+        EventRecord eventRecord = EventRecord.builder()
+                .event(event)
+                .member(member)
+                .participationTime(LocalDateTime.MAX)
+                .build();
+        eventRecordRepository.save(eventRecord);
+
+        // then
+        Optional<EventRecord> found = eventRecordRepository.findById(eventRecord.getId());
+        Assertions.assertThat(found).isPresent();
+        Assertions.assertThat(found.get().getParticipationTime()).isEqualTo(LocalDateTime.MAX);
+    }
+}
