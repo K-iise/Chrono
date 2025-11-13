de3bfaa20fa6d622af8eb15d12200cd2dd3b71f3
K-iise <91732817+K-iise@users.noreply.github.com>
2025-11-13 09:20:32 +0900

    feat: add registerMember

--- src/main/java/woowa/chrono/service/MemberService.java
+++ src/main/java/woowa/chrono/service/MemberService.java
@@ -1,4 +1,26 @@
 package woowa.chrono.service;
 
+import java.time.Duration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import woowa.chrono.domain.Grade;
+import woowa.chrono.domain.Member;
+import woowa.chrono.repository.MemberRepository;
+
+@Service
 public class MemberService {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    public Member registerMember(Member member) {
+        if (memberRepository.findByUserId(member.getUserId()).isPresent()) {
+            throw new IllegalArgumentException("이미 존재하는 멤버입니다.");
+        }
+        member.setGrade(Grade.NEWBIE);
+        member.setUsageTime(Duration.ZERO);
+        member.setPoint(0);
+        memberRepository.save(member);
+        return member;
+    }
 }
--- src/test/java/woowa/chrono/service/MemberServiceTest.java
+++ src/test/java/woowa/chrono/service/MemberServiceTest.java
@@ -4,11 +4,11 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import woowa.chrono.domain.Member;
 import woowa.chrono.repository.MemberRepository;
 
-@DataJpaTest
+@SpringBootTest
 public class MemberServiceTest {
     @Autowired
     private MemberService memberService;
