c93960fd0057bff6636cbb08c07a3d8b71e9a564
K-iise <91732817+K-iise@users.noreply.github.com>
2025-11-15 01:12:51 +0900

    test: add updateMemberGradeTest

--- src/test/java/woowa/chrono/service/MemberServiceTest.java
+++ src/test/java/woowa/chrono/service/MemberServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import woowa.chrono.domain.Grade;
 import woowa.chrono.domain.Member;
 import woowa.chrono.repository.MemberRepository;
 
@@ -20,13 +21,27 @@ public class MemberServiceTest {
     @DisplayName("멤버 등록 저장소에 저장된다.")
     public void registrationMemberTest() {
         // given
-        Member member = Member.builder().userId("123").userName("홍길동").build();
+        Member member = Member.builder().userId("1234").userName("홍길동").build();
 
         // when
         Member saved = memberService.registerMember(member);
 
         // then
         Assertions.assertThat(memberRepository.findById(saved.getId())).isPresent();
+    }
+
+    @Test
+    @DisplayName("멤버 등급을 변경한다")
+    public void updateMemberGradeTest() {
+        // given
+        Member member = Member.builder().userId("123").userName("홍길동").build();
+        memberRepository.save(member);
+
+        // when
+        Member found = memberService.updateMemberGrade("123", Grade.REGULAR);
+
+        // then
+        Assertions.assertThat(found.getGrade()).isEqualTo(Grade.REGULAR);
     }
 
 }
